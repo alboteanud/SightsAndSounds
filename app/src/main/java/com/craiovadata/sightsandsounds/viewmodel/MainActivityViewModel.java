8834bb5675eae3dc150fc5e3f600ac9bf1b0a5dc
albot <alboteanud@gmail.com>
2018-08-16 16:56:26 +0300

    Initial commit

---
+++ app/src/main/java/com/craiovadata/sightsandsounds/viewmodel/MainActivityViewModel.java
@@ -0,0 +1,36 @@
+package com.craiovadata.sightsandsounds.viewmodel;
+
+import android.arch.lifecycle.ViewModel;
+
+import com.craiovadata.sightsandsounds.Filters;
+
+/**
+ * ViewModel for {@link .MainActivity}.
+ */
+
+public class MainActivityViewModel extends ViewModel {
+
+    private boolean mIsSigningIn;
+    private Filters mFilters;
+
+    public MainActivityViewModel() {
+        mIsSigningIn = false;
+        mFilters = Filters.getDefault();
+    }
+
+    public boolean getIsSigningIn() {
+        return mIsSigningIn;
+    }
+
+    public void setIsSigningIn(boolean mIsSigningIn) {
+        this.mIsSigningIn = mIsSigningIn;
+    }
+
+    public Filters getFilters() {
+        return mFilters;
+    }
+
+    public void setFilters(Filters mFilters) {
+        this.mFilters = mFilters;
+    }
+}
